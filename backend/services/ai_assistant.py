c0e1b24b051501bb364f34590c9b4d55750769b2
Luka LaPointe <luka.lapointe@outlook.com>
2026-03-01 23:08:03 +0200

    langchain update

--- backend/services/ai_assistant.py
+++ backend/services/ai_assistant.py
@@ -9,11 +9,10 @@ import numpy as np
 import openai
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.retrievers import ParentDocumentRetriever
-from langchain.schema import Document
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from sqlalchemy import func
 from sqlalchemy.orm import Session
